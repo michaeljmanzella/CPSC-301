9ac9ec483c6aeaa06aab8694ebdba01af0995b1c
Michael Manzella <michaeljmanzella@outlook.com>
2017-09-25 01:02:13 -0700

    final 2

--- Text vs. Binary Files/Text vs. Binary Files/Source.cpp
+++ Text vs. Binary Files/Text vs. Binary Files/Source.cpp
@@ -14,22 +14,19 @@ void programSeven();
 
 int main()
 {
-	/*
 	progranOne();
 	programTwo();
 	programThree();
 	programFour();
-	*/
-	
-
-	//programFive();
-	//programSix();
+	programFive();
+	programSix();
 	programSeven();
 
 	return 0;
 }
 void progranOne()//one that opens a text file, write 2 integers to it, and closes it
 {
+	cout << "Program 1 -- one that opens a text file, write 2 integers to it, and closes it\n";
 	int num1 = 1;
 	double num2 = 2;
 	ofstream outFile;
@@ -44,6 +41,7 @@ void progranOne()//one that opens a text file, write 2 integers to it, and close
 }
 void programTwo()//one that opens the text file you wrote to, reads the 2 integers from it and prints them out to the screen
 {
+	cout << "Program 2 -- one that opens the text file you wrote to, reads the 2 integers from it and prints them out to the screen\n";
 	int num1;
 	int num2;
 	ifstream inFile;
@@ -60,6 +58,7 @@ void programTwo()//one that opens the text file you wrote to, reads the 2 intege
 }
 void programThree()//binary: one that opens a text file, write 2 integers to it, and closes it
 {
+	cout << "Program 3 -- binary: one that opens a text file, write 2 integers to it, and closes it\n";
 	char data[] = { '3','4' };
 	fstream file;
 
@@ -69,6 +68,7 @@ void programThree()//binary: one that opens a text file, write 2 integers to it,
 }
 void programFour()//binary: one that opens the text file you wrote to, reads the 2 numbers from it and prints them out to the screen
 {
+	cout << "Program 4 -- binary: one that opens the text file you wrote to, reads the 2 numbers from it and prints them out to the screen\n";
 	char data[2] = {};
 	fstream file;
 	
@@ -84,6 +84,7 @@ void programFour()//binary: one that opens the text file you wrote to, reads the
 }
 void programFive()//binary,classes: one that opens a text file, write 2 objects to it, and closes it
 {
+	cout << "Program 5 -- binary,classes: one that opens a text file, write 2 objects to it, and closes it\n";
 	Numbers testObjects[2];
 	//assign numbers to object
 	testObjects[0].integer = 1;
@@ -94,7 +95,7 @@ void programFive()//binary,classes: one that opens a text file, write 2 objects 
 	//create binary file
 	fstream outFile;
 	//open binary
-	outFile.open("testBinary.bin", ios::out | ios::binary);
+	outFile.open("testBinaryClasses.bin", ios::out | ios::binary);
 	//error check
 	if (outFile.fail())
 	{
@@ -106,11 +107,12 @@ void programFive()//binary,classes: one that opens a text file, write 2 objects 
 }
 void programSix()//binary classes: one that opens the text file you wrote to, reads the 2 classes from it and prints them out to the screen
 {
+	cout << "Program 6 -- binary classes: one that opens the text file you wrote to, reads the 2 classes from it and prints them out to the screen\n";
 	//create object
 	Numbers testObject[2];
 	fstream inFile;
 	//open binary inFile
-	inFile.open("testBinary.bin", ios::binary | ios::in);
+	inFile.open("testBinaryClasses.bin", ios::binary | ios::in);
 	//read binary file into array
 	inFile.read(reinterpret_cast<char *>(&testObject), sizeof(testObject));
 	//close binary
@@ -127,10 +129,11 @@ void programSeven()//Create an fstream to a binary file that you can both write 
 //and print them to the screen.  Next, see if you can retrieve the third object directly from the 
 //file by using seekg() and an offset from the file beginning of 2 object size. 
 {
+	cout << "Program 7 -- seek 2nd object in array\n";
 	Numbers testObjectsOut[5];
 	Numbers testObjectsIn;
 	int num1 = 1;
-	double num2 = 2.5;
+	double num2 = 1.5;
 	//assign numbers to object
 	for (int i = 0; i < 5; i++)
 	{
